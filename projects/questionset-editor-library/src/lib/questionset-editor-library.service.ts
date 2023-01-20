f81dfbb3d8b2535a6612a1ccadb78d164a98bcfe
Rajnish Dargan <rajnish.code30@gmail.com>
2023-01-20 16:57:22 +0530

    Issue #IQ-237 fix: test case fix

--- projects/questionset-editor-library/src/lib/questionset-editor-library.service.ts
+++ projects/questionset-editor-library/src/lib/questionset-editor-library.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class CollectionEditorLibraryService {
+export class QuestionsetEditorLibraryService {
 
   constructor() { }
 }
