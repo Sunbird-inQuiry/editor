85c5ca7c95e50d6b839d99eadba28996566b4907
Vaibhav Bhuva <vaibhav.bhuva@tarento.com>
2023-02-07 14:03:32 +0530

    Issue #IQ-236 feat: Removed library-filter component

--- projects/questionset-editor-library/src/lib/questionset-editor-library.module.ts
+++ projects/questionset-editor-library/src/lib/questionset-editor-library.module.ts
@@ -14,7 +14,6 @@ import { HeaderComponent } from './components/header/header.component';
 import { FancyTreeComponent } from './components/fancy-tree/fancy-tree.component';
 import { MetaFormComponent } from './components/meta-form/meta-form.component';
 import { LibraryComponent } from './components/library/library.component';
-import { LibraryFilterComponent } from './components/library-filter/library-filter.component';
 import { TemplateComponent } from './components/template/template.component';
 import { QumlplayerPageComponent } from './components/qumlplayer-page/qumlplayer-page.component';
 import { OptionsComponent } from './components/options/options.component';
@@ -59,7 +58,6 @@ import { QualityParamsModalComponent } from './components/quality-params-modal/q
     FancyTreeComponent,
     MetaFormComponent,
     LibraryComponent,
-    LibraryFilterComponent,
     QuestionComponent,
     OptionsComponent,
     AnswerComponent,
