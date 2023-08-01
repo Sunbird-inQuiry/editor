c19a96e59b7806000432ff2bf71910d429ef8020
Arpan Gupta <gupta-arpan@pop-os.localdomain>
2023-08-02 00:47:00 +0530

    Tests for match component

--- projects/questionset-editor-library/src/lib/components/match/match.component.spec.ts
+++ projects/questionset-editor-library/src/lib/components/match/match.component.spec.ts
@@ -1,20 +1,32 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TelemetryInteractDirective } from '../../directives/telemetry-interact/telemetry-interact.directive';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
 import { MatchComponent } from './match.component';
+import { CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import { ConfigService } from '../../services/config/config.service';
+import { SuiModule } from 'ng2-semantic-ui-v9';
+import { TreeService } from '../../services/tree/tree.service';
+import { EditorTelemetryService } from '../../services/telemetry/telemetry.service';
 
-describe('MatchComponent', () => {
+
+describe('OptionsComponent', () => {
   let component: MatchComponent;
   let fixture: ComponentFixture<MatchComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ MatchComponent ]
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, FormsModule, SuiModule],
+      declarations: [MatchComponent, TelemetryInteractDirective],
+      providers: [ConfigService, TreeService, EditorTelemetryService,],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
+  }));
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(MatchComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    // fixture.detectChanges();
   });
 
   it('should create', () => {
