69695082590ec170aceff8e85e8e5d3f1c75caee
pavankumar0408 <pavankumar.budharapu@tarento.com>
2024-01-22 14:19:51 +0530

    sonar cube test coverage

--- projects/questionset-editor-library/src/lib/components/meta-form/meta-form.component.spec.data.ts
+++ projects/questionset-editor-library/src/lib/components/meta-form/meta-form.component.spec.data.ts
@@ -418,7 +418,7 @@ export const mockData = {
                 createdBy: '5a587cc1-e018-4859-a0a8-e842650b9d64',
                 compatibilityLevel: 5,
                 navigationMode: 'non-linear',
-                evalMode: "server",
+                serverMode: true,
                 timeLimits: {
                     questionSet: {
                       min: 0,
