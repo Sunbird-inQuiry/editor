dea6d5fa0dfd455f68e403771d68e140792028df
Rakshitha-D <rakshithagowda.d@gmail.com>
2026-06-18 11:16:49 +0530

    fix: remove duplicate proxy routes from server.js catch-all block

--- server.js
+++ server.js
@@ -54,9 +54,8 @@ app.get(['/api/framework/v1/read/*',
 app.use(['/action/questionset/v2/*',
     '/action/question/v2/*',
     '/action/object/category/definition/v1/*',
-    '/api/object/category/definition/v1/*',
     '/api/question/v2/*',
-    '/api/questionset/v2/*'    ], proxy(BASE_URL, {
+    ], proxy(BASE_URL, {
     https: true,
     limit: '30mb',
     proxyReqPathResolver: function (req) {
